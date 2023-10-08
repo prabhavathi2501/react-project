3b2d6ce19b25b305c84c66e677927d6f02932036
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-08 19:16:04 +0530

    color change

--- reactproject/src/components/Sidebar.jsx
+++ reactproject/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ function sidebar() {
     <li className="nav-item active p-4  text-center">
      <h3>Notes App</h3>
       </li>
-     <li className="nav-item active bg-gradient-primary text-center">
+     <li className="nav-items active  border-radius:10px text-center">
     <h3>  <i className="fa-light fa-memo-pad" style={{ color: "#27282b;" }}></i>Notes </h3>
       </li>
    </ul>
